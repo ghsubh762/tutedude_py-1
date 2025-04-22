eb7a2ede699e60b112e8ecfd3ec5f70f8d4b6297
Shubh Gupta <102900626+ghsubh762@users.noreply.github.com>
2025-04-22 22:41:00 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,7 +1,11 @@
-# personalized greeting
+# task 2: personalized greeting
+
+# user inputs
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 
+# concatenation
 full_name = first_name + ' ' + last_name
 
+# display greeting
 print("Greetings, " + full_name + "! Welcome to this Python program.")
